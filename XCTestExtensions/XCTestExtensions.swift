8c424a28519e6d115762c725be9c0f48ec6f218e
Eugene <e@egeek.me>
2020-11-28 22:03:29 +0200

    WIP: Move `XCTAssertStringHasMinimalCount` to `XCTestExtensions`
    Warning: this commit doesn't build:
    
    ```
    XCTestExtensions/XCTestExtensions/XCTestExtensions.swift:18:17: error: use of unresolved identifier 'XCTUnwrap'
        let s = try XCTUnwrap(s, "Expected string to have at least \(minCount) characters, but it was nil: \(message())", file: file, line: line)
                    ^~~~~~~~~
    ```

--- XCTestExtensions/XCTestExtensions.swift
+++ XCTestExtensions/XCTestExtensions.swift
@@ -12,3 +12,9 @@ import XCTest
 public func XCTAssertWithinBounds <T: Comparable> (_ x: T, _ bounds: ClosedRange<T>, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) {
     XCTAssertTrue(bounds.contains(x), "Expected \(x) to be within the bounds \(bounds): \(message())", file: file, line: line)
 }
+
+/// Asserts that the string `s` is at least of the length `minCount`. If `s` is `nil`, the assertion fails.
+public func XCTAssertStringHasMinimalCount(_ s: String?, _ minCount: Int, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) throws {
+    let s = try XCTUnwrap(s, "Expected string to have at least \(minCount) characters, but it was nil: \(message())", file: file, line: line)
+    XCTAssertGreaterThanOrEqual(s.count, minCount, "Expected string \(s) to have at least \(minCount) characters, but it had \(s.count): \(message())", file: file, line: line)
+}
