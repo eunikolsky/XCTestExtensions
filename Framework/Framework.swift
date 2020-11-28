bf790a6028091427f58c51cb5170503370c38830
Eugene <e@egeek.me>
2020-11-28 21:07:38 +0200

    Initial commit

---
+++ Framework/Framework.swift
@@ -0,0 +1,6 @@
+//
+// Created by u on 28.11.20.
+// Copyright (c) 2020 yes. All rights reserved.
+//
+
+import Foundation
---
+++ FrameworkTests/FrameworkTests.swift
@@ -0,0 +1,13 @@
+//
+//  FrameworkTests.swift
+//  FrameworkTests
+//
+//  Created by u on 28.11.20.
+//  Copyright © 2020 yes. All rights reserved.
+//
+
+import XCTest
+import Framework
+
+class FrameworkTests: XCTestCase {
+}
